d5ab5e12dc55ab212b93fe83f17f9baa25421111
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-11-25 10:55:45 -0500

    revised love calculator with if/else statements

--- exercises.js
+++ exercises.js
@@ -22,3 +22,27 @@ getMilk(4);
 function bmiCalculator(weight, height) {
   return Math.round(weight / (height * height));
 }
+
+// function to create a "love calculator based on inputs"
+function loveCalc() {
+  var n = Math.random();
+   n = n * 100;
+   n = Math.floor(n) + 1;
+   promptOne = prompt("Enter the first name who wants love");
+   promptTwo = prompt("Enter the second name who wants love");
+   alert(promptOne + " and " + promptTwo + " have a " + n + " % love score");
+}
+loveCalc();
+
+// love calculator with if/else statements
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+    alert("Your love score is " + loveScore + "%" + " You love each other ver much!");
+}   else {
+    alert("Your love score is " + loveScore + "%");
+}
