1164c8af0670463f4735b416114a595c39757d5c
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-11-25 15:00:24 -0500

    function for fizzbuzz solution

--- exercises.js
+++ exercises.js
@@ -70,3 +70,25 @@ function isLeap(year) {
     return "Not leap year.";
   }
     }
+
+
+// fizzbuzz function
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } 
+  else if (count % 3 === 0) {
+    output.push("Fizz");
+  }
+   else if (count % 5 === 0) {
+    output.push("Buzz");
+  }
+  else {
+  output.push(count);
+  }
+  count++;
+  console.log(output);
+}
