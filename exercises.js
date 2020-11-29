410d81c36c7c163a38aa4596eae257b175b4d6cd
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-11-29 07:40:01 -0500

    function for random elements and fibonacci

--- exercises.js
+++ exercises.js
@@ -99,3 +99,38 @@ function whosPaying(names) {
   return names[Math.floor(Math.random()*names.length)];
 }
 whosPaying(names) + " is going to buy lunch today!";
+
+//function using while loop
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+
+//function to calculate fibonacci
+function fibonacciGenerator(n) {
+  var output = [];
+  if (n === 1) {
+    output = [0];
+  }
+  else if (n === 2) {
+    output = [0, 1];
+  }
+  else {
+    output = [0,1];
+  
+    for(var i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+   }
+  
+  return output;
+  }
+  fibonacciGenerator(10);
